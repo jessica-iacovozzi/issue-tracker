925c20b41821109dd3c1bdc1a5ef7ffd2919506b
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2024-01-10 09:55:32 -0500

    Styled navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,12 +1,18 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
 import { BsBugFill } from "react-icons/bs";
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const Navbar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' }
   ]
+
   return (
     <nav className='flex border-b px-6 space-x-6 items-center mb-6 h-14'>
       <Link href='/'><BsBugFill /></Link>
@@ -14,7 +20,11 @@ const Navbar = () => {
         {links.map(link =>
           <Link key={link.label}
                 href={link.href}
-                className=''>{link.label}</Link>)}
+                className={classnames({
+                  'text-gray-500': currentPath !== link.href,
+                  'text-gray-950': currentPath === link.href,
+                  'hover:text-gray-800 transition-colors': true
+                })}>{link.label}</Link>)}
       </ul>
     </nav>
   )
