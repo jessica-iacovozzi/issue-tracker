4328fab18e9de9eb2a5214284a4457ec15257e67
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2024-01-16 16:32:02 -0500

    Fixed query params bug

--- app/issues/list/IssuesTable.tsx
+++ app/issues/list/IssuesTable.tsx
@@ -20,42 +20,45 @@ const columns: { label: string, value: keyof Issue, className?: string }[] = [
 const IssuesTable = async ({ searchParams }: Props) => {
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+  const orderBy = columns.map(column => column.value)
+                         .includes(searchParams.orderBy)
+                         ? { [searchParams.orderBy]: 'asc' }
+                         : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
-    orderBy: {
-      [searchParams.orderBy]: 'asc'
-    }
+    orderBy
   });
 
   return (
     <Table.Root variant='surface'>
-        <Table.Header>
-          <Table.Row>
-            {columns.map(column => (
-              <Table.ColumnHeaderCell key={column.value} className={column.className} width='33%'>
-                <Link href={{
-                  query: { ...searchParams, orderBy: column.value }
-                }}>{column.label}</Link>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className='inline align-text-bottom ms-1' />}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map(issue => (
-            <Table.Row key={issue.id}>
-              <Table.Cell width='33%'>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className='block md:hidden'>
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell width='33%' className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-              <Table.Cell width='33%' className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
-            </Table.Row>
+      <Table.Header>
+        <Table.Row>
+          {columns.map(column => (
+            <Table.ColumnHeaderCell key={column.value} className={column.className} width='33%'>
+              <Link href={{
+                query: { ...searchParams, orderBy: column.value }
+              }}>{column.label}</Link>
+              {column.value === searchParams.orderBy && <ArrowUpIcon className='inline align-text-bottom ms-1' />}
+            </Table.ColumnHeaderCell>
           ))}
-        </Table.Body>
-      </Table.Root>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map(issue => (
+          <Table.Row key={issue.id}>
+            <Table.Cell width='33%'>
+              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              <div className='block md:hidden'>
+                <IssueStatusBadge status={issue.status} />
+              </div>
+            </Table.Cell>
+            <Table.Cell width='33%' className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
+            <Table.Cell width='33%' className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
   )
 }
 
