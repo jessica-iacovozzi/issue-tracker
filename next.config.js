5a931368cbcc9a393a227db6a1b714141bb5b4dc
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2024-01-16 10:03:12 -0500

    Added prisma adapters + related db tables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async header() {
+    return [
+      {
+        source: '/:path*', // Affects all endpoints
+        headers: [
+          { key: 'referrer-policy', value: 'no-referrer' } // To fix rendering of google avatar
+        ]
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
