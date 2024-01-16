72b87a1ba9e120c279aa180b7c334a72acd2cc69
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2024-01-16 09:04:08 -0500

    Added middleware to protect new issue route

--- app/auth/Provider.tsx
+++ app/auth/Provider.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import React, { ReactNode } from 'react';
+import React, { PropsWithChildren } from 'react';
 import { SessionProvider } from 'next-auth/react';
 
-const AuthProvider = ({ children }: { children: ReactNode }) => {
+const AuthProvider = ({ children }: PropsWithChildren) => {
   return (
     <SessionProvider>{children}</SessionProvider>
   )
