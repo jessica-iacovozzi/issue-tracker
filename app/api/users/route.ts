3ab247865f04d0d26def024882dc2dac308dc289
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2024-01-16 10:56:54 -0500

    Created assignee select component

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,6 +3,5 @@ import prisma from "@/prisma/client";
 
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany({ orderBy: { name: 'asc' }});
-
   return NextResponse.json(users);
 }
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,29 @@
 'use client';
 
+import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Skeleton } from "@/app/components";
 
 const AssigneeSelect = () => {
+  const { data: users, error, isLoading } = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000, // 60s
+    retry: 3
+  });
+
+  if (isLoading) return <Skeleton />
+  if (error) return null;
+
   return (
     <Select.Root>
       <Select.Trigger placeholder='Assign...' />
       <Select.Content>
         <Select.Group>
           <Select.Label>Users</Select.Label>
-          <Select.Item value="1">Jessica Iacovozzi</Select.Item>
+          {users?.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
         </Select.Group>
       </Select.Content>
     </Select.Root>
