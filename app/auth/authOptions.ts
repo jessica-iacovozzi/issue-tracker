c20ecb6bd1d283522b626827fca605312483ded5
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2024-02-07 22:03:22 -0500

    Added github login

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github"
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -9,6 +10,10 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!
     })
   ],
   session: {
